c7cc1e21dea5f0deda9b1737dcd86564cee23557
kansi <kansi13@gmail.com>
2017-11-16 22:22:08 +0530

    Added backend test, parameterize app server tests

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 
+from bigchaindb.tendermint.lib import BigchainDB
 
-@pytest.fixture
+
+@pytest.fixture(params=[None, BigchainDB])
 def app(request):
     from bigchaindb.web import server
-    app = server.create_app(debug=True)
+    app = server.create_app(debug=True, bigchaindb_factory=request.param)
     return app
